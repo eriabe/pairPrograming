3df9276250bc085a91d0f20ea55038c6771ae83a
chisae <g13908ic+git@gm.tsuda.ac.jp>
2015-05-08 13:39:48 +0900

    ドライバー：岩科 ナビゲーター：阿部 3桁の変換をメソッドを使って完成

--- pairPrograming/src/pairPrograming/IntToEng.java
+++ pairPrograming/src/pairPrograming/IntToEng.java
@@ -4,38 +4,46 @@ import java.util.Scanner;
 public class IntToEng {
 	public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
-
-        System.out.println(translateEng(input));
-
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();  
+        System.out.println(translateEng(input)); 
     }
 
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
     	String answer = "";
+    	if(n/100 > 0){//100~999
+    		return third(n);
+    	} else if (n/10 > 0) {
+    		return second(n); 		
+    	} else {
+    		return first(n);
+    	}
+    }
+    static String first(int n) {
     	String[] first = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    	return first[n];   
+    }
+    static String second(int n) {
     	String[] second1 = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
     	String[] second2 = {"","","twenty", "thirty", "forty", "fifty", "sixth", "seventy", "eighty", "ninety"};
-    	
-    	if(n/100 > 0){//100~999
-    		if((n%100)==0){
-    			answer = first[n/100]+"hundred";
-    		}else if((n%100)%10==0){
-    			answer = first[n/100]+"hundred "+second2[(n%100)/10];
-    		}else{
-    		answer = first[n/100]+"hundred "+second2[(n%100)/10] +" "+first[(n%100)%10];
-    		}
-    	} else if (n/10 == 1) {//10~19
-    		answer = second1[n%10];
+    	if (n/10 == 1) {//10~19
+    		return  second1[n%10];
     	} else if (n/10 > 1) {//20~90
-    		answer = second2[n/10] +" " +first[n%10];
-      	}else if (n/10 == 0) {//0~9
-    		answer = first[n];
+    		if (n%10 == 0) {
+    			return second2[n/10];
+    		}else{
+    		return second2[n/10] +" " +first(n%10);}
     	} else {   		
-    		answer = "";
+    		return first(n%10);
     	}
-        return answer;
+    }
+    static String third(int n) {
+    	if((n%100)==0){
+			return first(n/100)+"hundred";
+		}else{
+			return first(n/100)+"hundred "+second(n%100);
+		}
     }
 }
 
